07be4943b68e06edd45d2ace79d95cc512065668
Jhon Livias <jhonlivias3@gmail.com>
2023-04-07 08:52:16 -0500

    Add middleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 # from pydantic import BaseModel
 # from typing import List
 # from selenium import webdriver
@@ -7,6 +8,15 @@ app = FastAPI()
 # driver = webdriver.Chrome(executable_path="C:\chromedriver\chromedriver.exe")
 # driver.get("http://python.org")
 # driver.close()
+
+# Permitir el acceso desde cualquier origen
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     pass

4246bcac228f49b50d5afea20b7e50480b49f707
Jhon Livias <jhonlivias3@gmail.com>
2023-04-06 19:43:11 -0500

    Initial project

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, HTTPException
+# from pydantic import BaseModel
+# from typing import List
+# from selenium import webdriver
+
+app = FastAPI()
+# driver = webdriver.Chrome(executable_path="C:\chromedriver\chromedriver.exe")
+# driver.get("http://python.org")
+# driver.close()
+
+if __name__ == "__main__":
+    pass
